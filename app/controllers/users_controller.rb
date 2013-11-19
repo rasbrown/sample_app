c6a42906191c2978cf74d691ab75d2e385444bc8
Rebecca Brown <becky.swanson@gmail.com>
2013-11-19 13:17:42 -0500

    Add sign in and out functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
@@ -19,7 +20,7 @@ class UsersController < ApplicationController
   
   private
   
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
