a80d99bfae28d8169fb16e985d6085269f0cdca2
Rebecca Brown <becky.swanson@gmail.com>
2013-11-22 10:35:17 -0500

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,44 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :anon_user, only: [:new, :create]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+  
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+  
   def show
     @user = User.find(params[:id])
   end
   
+  def destroy
+    delete_user = User.find(params[:id])
+    if current_user?(delete_user)
+      flash[:error] = "Can't delete your own account"
+    else
+      delete_user.destroy
+      flash[:success] = "User deleted."
+    end
+    redirect_to users_url
+  end
+  
   def new
     @user = User.new
+  end
+  
+  def edit
+    #@user = User.find(params[:id]) don't need this (in correct_user before_action)
+  end
+  
+  def update
+    #@user = User.find(params[:id]) don't need this (in correct_user before_action)
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
   
   def create
@@ -22,5 +56,27 @@ class UsersController < ApplicationController
   
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+    
+    # Before filters
+    
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+    
+    def anon_user
+       redirect_to(root_url) if signed_in?
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+    
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 end
